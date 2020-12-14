ac6e48a35a9456ab2de0a6d66c3ed5f188ce9c70
Jorge <jorgealen445@gmail.com>
2020-12-13 23:24:13 -0300

    primer comit

---
+++ javas.js
@@ -0,0 +1,28 @@
+var d = document.getElementById("canvas-items");
+var ellienzo = d.getContext("2d");
+var lineas = 50;
+var l = 0;
+var yi, xf;
+var colora = "rgb(81, 255, 0)";
+
+for(l=0; l < lineas; l++)
+{
+    yi = 10 * l;
+    xf = 10 * (l + 1);
+    dibujarLinea(colora, 500, yi, xf, 0);
+    console.log("linea" + 1);
+}
+
+dibujarLinea(colora, 499,1,0,1);
+dibujarLinea(colora, 499,1,499,499);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    ellienzo.beginPath();
+    ellienzo.strokeStyle = color;
+    ellienzo.moveTo(xinicial, yinicial);
+    ellienzo.lineTo(xfinal, yfinal);
+    ellienzo.stroke();
+    ellienzo.closePath();
+}
+
